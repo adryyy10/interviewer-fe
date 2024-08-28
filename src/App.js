254d38fde21fa094146efe700e9f489aae53c72c
adriafigueres <adria.figueres@m10c.com>
2024-08-28 11:34:23 +0100

    feat: show correct/incorrect answer when clicking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import QuestionPage from './components/QuestionPage';
+import './App.css';
 
 function App() {
     return (
-        <>
+        <div className="app-container">
             <QuestionPage />
-        </>
+        </div>
     );
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Question.css';
 
 const Question = ({ question }) => {
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [isCorrect, setIsCorrect] = useState(null);
+
+    const handleAnswerClick = (answer) => {
+        setSelectedAnswer(answer);
+        setIsCorrect(answer.isCorrect);
+    };
+
     return (
         <div className="question-container">
             <h3 className="question-title">{question.content}</h3>
             <ul className="question-answers">
                 {question.answers.map((answer, index) => (
-                    <li key={index}>{answer.content}</li>
+                    <li 
+                        key={index} 
+                        className={`answer ${selectedAnswer === answer ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
+                        onClick={() => handleAnswerClick(answer)}
+                    >
+                        {answer.content}
+                    </li>
                 ))}
             </ul>
         </div>
