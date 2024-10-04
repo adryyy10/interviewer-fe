843afa6534166eb432b90062c155cae1b87e456c
adriafigueres <adria.figueres@m10c.com>
2024-10-04 07:41:51 +0100

    fix: broken access control for non admin users

--- src/App.js
+++ src/App.js
@@ -7,43 +7,32 @@ import AdminQuestions from './components/AdminQuestions';
 import AdminUsers from './components/AdminUsers';
 import AuthProvider from './hooks/AuthProvider';
 import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 
 const App = () => {
-
     return (
         <Router>
             <AuthProvider>
                 <Routes>
+                    {/* Public Logged Routes */}
+                    <Route element={<PublicRoute />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Route>
+
+                    {/* Admin Logged Routes */}
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/admin/questions" element={<AdminQuestions />} />
+                        <Route path="/admin/questions/create" element={<CreateQuestionForm />} />
+                        <Route path="/admin/users" element={<AdminUsers />} />
+                    </Route>
+
+                    {/* Other Routes */}
                     <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route element={<PrivateRoute />}>
-                        <Route
-                            path="/dashboard"
-                            element={<Dashboard /> }
-                        />
-                    </Route>
-                    <Route element={<PrivateRoute />}>
-                        <Route
-                            path="/admin/questions"
-                            element={<AdminQuestions /> }
-                        />
-                    </Route>
-                    <Route element={<PrivateRoute />}>
-                        <Route
-                            path="/admin/questions/create"
-                            element={<CreateQuestionForm /> }
-                        />
-                    </Route>
-                    <Route element={<PrivateRoute />}>
-                        <Route
-                            path="/admin/users"
-                            element={<AdminUsers /> }
-                        />
-                    </Route>
                     <Route path="/questions" element={<QuestionPage />} />
                 </Routes>
             </AuthProvider>
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,8 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/AuthProvider";
 
 const PrivateRoute = () => {
-    const user = useAuth();
-    if (!user.token) return <Navigate to="/login" />;
+    const { token, admin } = useAuth();
+    if (!token) return <Navigate to="/login" />;
+    if (!admin) return <Navigate to="/" />;
     return <Outlet />;
 };
 
